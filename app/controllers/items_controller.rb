58d4e8a6102957ec50a1b0fbcf0577fcb75794a3
farhan <farhanj.mohamed@gmail.com>
2022-12-20 14:10:42 -0800

    added actions to items controller and routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,42 @@ class ItemsController < ApplicationController
   end
 
   def create
+    item = Item.new(name: params[:name])
+
+    item.save
+
+    render json: item.as_json
   end
 
   def show
+    id = params[:id]
+    item = Item.find(id)
+
+    render json: item.as_json
   end
 
   def update
+    id = params[:id]
+    item = Item.find(id)
+
+    item.update( 
+      item.name = params[:name] || item.name
+      item.description = params[:description] || item.description
+      item.img_url = params[:img_url] || item.img_url
+      item.category_id = params[:category_id] || item.category_id
+    )
+    item.save
+
+    render json: item.as_json
+
   end
 
   def destroy
+    id = params[:id]
+    item = Item.find(id)
+
+    item.destroy
+
+    render json: {message: "you've successfully deleted this item!"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
 
   ### Sessions Routes ###
   post "/sessions" => "sessions#create"
+
+  ## Item Routes ###
+  get "/items" => "items#index"
+  get "/items/:id" => "items#show"
+  post "/items" => "items#create"
+  patch "/items/i:d" => "items#update"
+  delete "/items/:id" => "items#destroy"
 end
